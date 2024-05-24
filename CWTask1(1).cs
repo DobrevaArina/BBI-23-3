af96b9666f0e123bf09568709509d2f2d38872e1
DobrevaArina <159530849+DobrevaArina@users.noreply.github.com>
2024-05-24 14:08:22 +0300

    Add files via upload

---
+++ CWTask1(1).cs
@@ -0,0 +1,84 @@
+﻿public class Book
+{
+    public string Title { get; }
+    public int ISBN { get;}
+    public string Author { get;}
+    public int PublicationYear { get;}
+    private static int isbn = 0;
+
+    public Book(string title, string author, int publicationYear)
+    {
+        Title = title;
+        isbn++;
+        ISBN = isbn;
+        Author = author;
+        PublicationYear = publicationYear;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Publication Year: {PublicationYear}");
+        Console.WriteLine();
+    }
+
+    public static void PrintBooksByAuthor(Book[] books, string author)
+    {
+        Console.WriteLine($"Книги автора {author}:");
+        foreach (Book book in books)
+        {
+            if (book.Author == author)
+            {
+                book.PrintInfo();
+            }
+        }
+    }
+
+    public static void PrintBooksByCentury(Book[] books, int century)
+    {
+        Console.WriteLine($"Книги {century}-го века:");
+        foreach (Book book in books)
+        {
+            if ((book.PublicationYear / 100)+1 == century)
+            {
+                book.PrintInfo();
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        Book[] books = new Book[10]
+        {
+            new Book("Вино из одуванчиков", "Брэдбери", 1979),
+            new Book("1984", "Оруэл", 1954),
+            new Book("Приют грёз", "Ремарк", 1949),
+            new Book("Капиатнская дочка", "Пушкин", 1860),
+            new Book("Маленький прицн", "Экзюпери", 1625),
+            new Book("Герой нашего времени", "Лермонтов", 1500),
+            new Book("Белые ночи", "Достоевский", 1825),
+            new Book("Война и мир", "Толстой", 1825),
+            new Book("Яма", "Куприн", 1650),
+            new Book("Скотный двор", "Оруэл", 1970),
+        };
+         
+        string author = "Оруэл";
+        int century = 20;
+
+        Console.WriteLine("Информация о всех книгах:");
+        foreach (Book book in books)
+        {
+            book.PrintInfo();
+        }
+
+        Book.PrintBooksByAuthor(books, author);
+
+        Book.PrintBooksByCentury(books, century);
+    }
+}
---
+++ CWTask2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class Shape
+{
+    public abstract double Perimeter { get; }
+    public abstract double Area { get; }
+}
+
+public class Round : Shape
+{
+    public double Radius { get; }
+
+    public Round(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double Perimeter => 2 * Math.PI * Radius;
+
+    public override double Area => Math.PI * Radius * Radius;
+}
+
+public class Square : Shape
+{
+    public double Side { get; }
+
+    public Square(double side)
+    {
+        Side = side;
+    }
+
+    public override double Perimeter => 4 * Side;
+
+    public override double Area => Side * Side;
+}
+
+public class Triangle : Shape
+{
+    public double SideA { get; }
+    public double SideB { get; }
+    public double SideC { get; }
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+
+    public override double Perimeter => SideA + SideB + SideC;
+
+    public override double Area
+    {
+        get
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var roundShapes = new List<Round>
+        {
+            new Round(5),
+            new Round(10),
+            new Round(15),
+            new Round(20),
+            new Round(25)
+        };
+
+        var squareShapes = new List<Square>
+        {
+            new Square(5),
+            new Square(10),
+            new Square(15),
+            new Square(20),
+            new Square(25)
+        };
+
+        var triangleShapes = new List<Triangle>
+        {
+            new Triangle(3, 4, 5),
+            new Triangle(5, 10, 12),
+            new Triangle(7, 8, 9),
+            new Triangle(9, 10, 11),
+            new Triangle(11, 12, 13)
+        };
+
+        PrintSortedShapes(roundShapes);
+
+        Console.WriteLine();
+
+        PrintSortedShapes(squareShapes);
+
+        Console.WriteLine();
+
+        PrintSortedShapes(triangleShapes);
+
+        Console.WriteLine();
+
+        var combinedShapes = roundShapes.Concat(squareShapes).Concat(triangleShapes);
+
+        PrintSortedShapes(combinedShapes.OrderByDescending(s => s.Area));
+    }
+
+    private static void PrintSortedShapes<TShape>(IEnumerable<TShape> shapes)
+        where TShape : Shape
+    {
+        Console.WriteLine($"{typeof(TShape).Name}s:");
+
+        Console.WriteLine("ShapettttPerimetertArea");
+        Console.WriteLine("--------------------------------------------");
+
+        foreach (var shape in shapes.OrderByDescending(s => s.Area))
+        {
+            Console.WriteLine($"{shape.GetType().Name}tttt{shape.Perimeter}t{shape.Area}");
+        }
+    }
+}
